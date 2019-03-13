b3aee26e413413b33b401ea22140305adad254ee
kolja <koljaz@web.de>
2019-03-13 21:42:44 +0100

    Made sure multigraph is transformed to JSON properly as according to spec.

--- test/testJsonDecorator.js
+++ test/testJsonDecorator.js
@@ -48,8 +48,69 @@ describe('JsonDecorator', () => {
             });
         });
 
-        xit('should transform multigraph to json', () => {
+        it('should transform multigraph to json', () => {
+            let multigraph = new JgfMultiGraph('weird-multigraph', 'This is weird', { weirdness: 100 });
 
+
+            let graph = new JgfGraph('someType', 'someLabel', true, { bla: 'some-meta-data' });
+
+            graph.addNode(new JgfNode('firstNodeId', 'blubb-label', { bla: 'whoopp' }));
+            graph.addNode(new JgfNode('secondNodeId', 'bla-label', { bli: 'whaaat' }));
+
+            graph.addEdge(new JgfEdge('firstNodeId', 'secondNodeId', 'is-test-edge', 'edge-label', { meta: 'edge-metadata' }, true));
+
+            multigraph.addGraph(graph);
+
+
+            graph = new JgfGraph('otherType', 'otherLabel', false, { ble: 'some-blumeta-data' });
+
+            graph.addNode(new JgfNode('other-firstNodeId', 'effe-label', { ufe: 'schnad' }));
+            graph.addNode(new JgfNode('other-secondNodeId', 'uffe-label', { bame: 'bral' }));
+
+            graph.addEdge(new JgfEdge('other-firstNodeId', 'other-secondNodeId', 'is-ither-test-edge', 'other-edge-label', { other_meta: 'otheredge-metadata' }, false));
+
+            multigraph.addGraph(graph);
+
+            assert.deepEqual(JgfJsonDecorator.toJson(multigraph), {
+                graphs: [
+                    {
+                        type: "someType",
+                        label: "someLabel",
+                        directed: true,
+                        metadata: { bla: "some-meta-data" },
+                        nodes: [
+                            {id: "firstNodeId", label: "blubb-label", metadata: { bla: "whoopp" }},
+                            {id: "secondNodeId", label: "bla-label", metadata: { bli: "whaaat" }},
+                        ],
+                        edges: [{
+                            source: "firstNodeId",
+                            target: "secondNodeId",
+                            relation: "is-test-edge",
+                            label: "edge-label",
+                            metadata: { meta: "edge-metadata" },
+                            directed: true,
+                        }],
+                    },
+                    {
+                        type: "otherType",
+                        label: "otherLabel",
+                        directed: false,
+                        metadata: { ble: "some-blumeta-data"},
+                        nodes: [
+                            { id: "other-firstNodeId", label: "effe-label", metadata: { ufe: "schnad" } },
+                            { id: "other-secondNodeId", label: "uffe-label", metadata: { bame: "bral" } },
+                        ],
+                        edges: [{
+                            source: "other-firstNodeId",
+                            target: "other-secondNodeId",
+                            relation: "is-ither-test-edge",
+                            label: "other-edge-label",
+                            metadata: { other_meta: "otheredge-metadata" },
+                            directed: false,
+                        }],
+                    }
+                ]
+            });
         });
     });
 });
