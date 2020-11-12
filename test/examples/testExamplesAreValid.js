07c7cf0ab152273052b2a29b2bb0fa3592e3f063
ArSn <koljaz@web.de>
2020-11-12 23:52:29 +0100

    Simplified example file schema checking test a lot. Grabbing all valid files now.

--- test/examples/testExamplesAreValid.js
+++ test/examples/testExamplesAreValid.js
@@ -4,53 +4,32 @@ const fs = require('fs');
 
 const schemaPath = __dirname + '/../../jgfSchemaV1.json';
 
+// todo: add test for invalid example (and some more tests)
 const invalidExamples = [
   'bad_car_graphs.json',
 ];
 
 const validExamples = [
   'car_graphs.json',
+  'les_miserables.json',
+  'nba-players.json',
+  'nba-teams.json',
+  'test.network.json',
+  'usual_suspects.json',
 ];
 
 describe('v1 Test Examples', () => {
-  describe('All files', () => {
-    it('should be valid according to the schema', () => {
+  describe('Files should be valid according to the schema', () => {
 
-      let schema;
-      fs.readFile( schemaPath, function (err, data) {
-        if (err) {
-          throw err;
-        }
-        schema = JSON.parse(data);
+    const ajv = new Ajv();
+    const validator = ajv.compile(require(schemaPath));
 
-        var ajv = new Ajv();
-
-
-        // ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'));
-        const validator = ajv.compile(schema);
-
-
-        fs.readFile( __dirname + '/v1/car_graphs.json', function (err, data) {
-          if (err) {
-            throw err;
-          }
-          // console.log(data.toString());
-
-          assert.isTrue(validator(JSON.parse(data)));
-        });
+    validExamples.forEach((filename) => {
+      it('file ' + filename, () => {
+        const example = require(__dirname + '/v1/' + filename);
+        assert.isTrue(validator(example));
       });
     });
-
-    // it('should allow omitting optional parameters', () => {
-    //   let edge = new JgfEdge('earth', 'moon');
-    //
-    //   assert.equal(edge.source, 'earth');
-    //   assert.equal(edge.target, 'moon');
-    //   assert.isNull(edge.relation);
-    //   assert.isNull(edge.label);
-    //   assert.isNull(edge.metadata);
-    //   assert.isTrue(edge.directed);
-    // });
   });
 
 });
